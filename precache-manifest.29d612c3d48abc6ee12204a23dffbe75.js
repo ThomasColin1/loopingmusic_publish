0bfb9293e16be8e8b562cf6b642e5287ac3a5f93
Thomas Colin <79255127+ThomasColin1@users.noreply.github.com>
2022-06-12 17:11:21 +0200

    publish v1

---
+++ precache-manifest.29d612c3d48abc6ee12204a23dffbe75.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b28d83517841db0066f3",
+    "url": "/static/css/main.eb624ff7.chunk.css"
+  },
+  {
+    "revision": "b28d83517841db0066f3",
+    "url": "/static/js/main.b28d8351.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "de05776d6205fe938b8a",
+    "url": "/static/js/2.de05776d.chunk.js"
+  },
+  {
+    "revision": "39a7286a869c5b2ec7f4",
+    "url": "/static/js/runtime~main.39a7286a.js"
+  },
+  {
+    "revision": "cac6f4f6ddbe92403ef75aab346d1f59",
+    "url": "/static/media/Loading_2.cac6f4f6.gif"
+  },
+  {
+    "revision": "18085aa344c9f7f01f7b0285861006ef",
+    "url": "/static/media/feuille1.18085aa3.png"
+  },
+  {
+    "revision": "3d2a9fd5dab9acb86dcf418b84ffd78e",
+    "url": "/static/media/feuille2.3d2a9fd5.png"
+  },
+  {
+    "revision": "080913d7923ede2a97f11ce1f93c0b23",
+    "url": "/static/media/Melody.080913d7.wav"
+  },
+  {
+    "revision": "554f53d892ead316521a65320d68932e",
+    "url": "/static/media/chords.554f53d8.wav"
+  },
+  {
+    "revision": "9bcbed169802f265ac1d846d8a267bc8",
+    "url": "/static/media/chopchords.9bcbed16.wav"
+  },
+  {
+    "revision": "bca94eeefa8f0b68c9275a3f99f1b8de",
+    "url": "/static/media/kicksnare.bca94eee.wav"
+  },
+  {
+    "revision": "162dc3cde9d5963310b1685ea752f5c8",
+    "url": "/static/media/bass.162dc3cd.wav"
+  },
+  {
+    "revision": "8e988f58fbfd7af4a7c3870edd373e1e",
+    "url": "/static/media/ride.8e988f58.wav"
+  },
+  {
+    "revision": "b58408ca62c86a9df7ec2aa0f87fe898",
+    "url": "/static/media/violin.b58408ca.wav"
+  },
+  {
+    "revision": "7ec9c61723d65a34344bd7dc866763da",
+    "url": "/static/media/clock.7ec9c617.wav"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5d6936a6b5322eaae18827e298ec423a",
+    "url": "/index.html"
+  }
+];
